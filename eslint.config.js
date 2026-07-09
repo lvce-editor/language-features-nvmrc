ffcc11fa1524860cdba435cb4f6b578dea83bd0e
Le Vivilet <levivilet@pm.me>
2026-07-09 10:06:23 +0200

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,4 @@
 import * as config from '@lvce-editor/eslint-config'
 import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [
-  ...config.default,
-  ...actions.default,
-  {
-    rules: {
-      'github-actions/ci-versions': 'off',
-      'unicorn/no-top-level-side-effects': 'off',
-      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
-      'no-useless-escape': 'off',
-    },
-  },
-]
+export default [...config.default, ...actions.default]
